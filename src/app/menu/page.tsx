be5560720848f0c749dc4479b7093b6d9609d8df
Goudmu <goudmu@gmail.com>
2023-11-17 21:08:15 +0700

    menu page

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -2,19 +2,19 @@ import { Menu } from '@/types/type'
 import Link from 'next/link'
 import React, { cache } from 'react'
 
-const getData =async () => {
-  const res = await fetch("http://localhost:3000/api/categories", {
-    cache : "no-store"
-  })
-
-  if(!res.ok){
-    throw new Error("Failed")
-  }
-
-  return res.json()
-}
 
 const MenuPage = async () => {
+  const getData =async () => {
+    const res = await fetch("http://localhost:3000/api/categories", {
+      cache : "no-store"
+    })
+  
+    if(!res.ok){
+      throw new Error("Failed")
+    }
+  
+    return res.json()
+  }
   const menu:Menu = await getData()
 
   return (
