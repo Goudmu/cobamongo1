915d79dced3e52d151e33ff725edc7c0419e5e78
Goudmu <goudmu@gmail.com>
2023-11-21 11:48:24 +0700

    add loading

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from 'react'
 
 const MenuPage = () => {
   const [menu, setMenu] = useState<Menu>([]);
+  const [isLoading, setisLoading] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
@@ -14,30 +15,46 @@ const MenuPage = () => {
       }).then(res => res.json())
       .then(data => {
         setMenu(data.catGet)
+        setisLoading(false)
       })
     }
     getData()
   },[])
 
-  return (
-    <div className='md:min-h-[calc(100vh-15rem)]' >
-      <div className='p-4 lg:px-20 xl:px-40
-      md-[calc(100vh-9rem)] gap-4 grid grid-row-1 md:grid-cols-2'>
-        {menu.map(category => (
-          <Link key={category._id} href={`/menu/${category.cat}`} 
-          className='w-full bg-cover p-8 rounded-md md:transform md:transition md:duration-500 md:hover:scale-105' 
-          style={{backgroundImage:`url(${category.img})`}}
-          >
-            <div className={`text-${category.color} w-1/2`} >
-              <h1 className='uppercase font-bold text-3xl' >{category.title}</h1>
-              <p className='text-sm my-6' >{category.desc}</p>
-              <button className={`hidden 2xl:block bg-[#04979e] text-${category.color === "white" ? "white" : "black"} py-2 px-4 rounded-md `}>Explore</button>
-            </div>
-          </Link>
-        ))}
+  if(isLoading){
+    return(
+      <div className='w-screen h-screen grid place-items-center' >
+          <div
+            className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+            role="status">
+            <span
+              className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+              >Loading...</span>
+          </div>
+        </div>
+    )
+  } else {
+    return (
+      <div className='md:min-h-[calc(100vh-15rem)]' >
+        <div className='p-4 lg:px-20 xl:px-40
+        md-[calc(100vh-9rem)] gap-4 grid grid-row-1 md:grid-cols-2'>
+          {menu.map(category => (
+            <Link key={category._id} href={`/menu/${category.cat}`} 
+            className='w-full bg-cover p-8 rounded-md md:transform md:transition md:duration-500 md:hover:scale-105' 
+            style={{backgroundImage:`url(${category.img})`}}
+            >
+              <div className={`text-${category.color} w-1/2`} >
+                <h1 className='uppercase font-bold text-3xl' >{category.title}</h1>
+                <p className='text-sm my-6' >{category.desc}</p>
+                <button className={`hidden 2xl:block bg-[#04979e] text-${category.color === "white" ? "white" : "black"} py-2 px-4 rounded-md `}>Explore</button>
+              </div>
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
+
 }
 
 export default MenuPage
--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from 'react'
 
 const Featured = () => {
   const [featuredProducts, setfeaturedProducts] = useState<Product[]>([]);
+  const [isLoading, setisLoading] = useState(true);
   const router = useRouter()
 
   useEffect(() => {
@@ -15,37 +16,53 @@ const Featured = () => {
       }).then(res => res.json())
       .then(data => {
         setfeaturedProducts(data.productss)
+        setisLoading(false)
       })
     }
     getData()
   },[])
   
-  return (
-    <div className='w-full overflow-x-scroll text-[#04979e]' >
-      <div className='w-max flex' >
-        {
-          featuredProducts.map(item => {
-            if(item.isFeatured == true) {
-              return (
-              <div key={item._id} className='w-screen md:w-[50vw] xl:w-[33vw] h-[60vh] xl:h[90vh] flex flex-col items-center justify-around p-4 hover:bg-[#DCF1F1] transition-all duration-150 ' >
-              { item.img && (
-                <div className='relative flex-1 w-full hover:rotate-[30deg] transition-all duration-500 '>
-                  <Image src={item.img} alt='' fill className='object-contain' sizes='100%' priority={true} />
-                </div>
-              )}
-              <div className='flex-1 flex flex-col gap-4 items-center text-center justify-center' >
-                <h1 className='text-xl xl:text-2xl 2xl:text-3xl font-bold uppercase ' >{item.title}</h1>
-                <p className='p-4 2xl:p-8 '>{item.desc}</p>
-                <span className='text-xl font-bold' >${item.price}</span>
-                <button className='bg-[#04979e] text-white p-2 rounded-md' onClick={() => router.push(`/product/${item._id}`)} >ADD TO CART</button>
-              </div>
-            </div>
-            )
-          }
-        })}
+  if(isLoading){
+    return (
+      <div className='w-screen h-screen grid place-items-center' >
+        <div
+          className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
+          role="status">
+          <span
+            className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+            >Loading...</span>
+        </div>
       </div>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div className='w-full overflow-x-scroll text-[#04979e]' >
+        <div className='w-max flex' >
+          {
+            featuredProducts.map(item => {
+              if(item.isFeatured == true) {
+                return (
+                <div key={item._id} className='w-screen md:w-[50vw] xl:w-[33vw] h-[60vh] xl:h[90vh] flex flex-col items-center justify-around p-4 hover:bg-[#DCF1F1] transition-all duration-150 ' >
+                { item.img && (
+                  <div className='relative flex-1 w-full hover:rotate-[30deg] transition-all duration-500 '>
+                    <Image src={item.img} alt='' fill className='object-contain' sizes='100%' priority={true} />
+                  </div>
+                )}
+                <div className='flex-1 flex flex-col gap-4 items-center text-center justify-center' >
+                  <h1 className='text-xl xl:text-2xl 2xl:text-3xl font-bold uppercase ' >{item.title}</h1>
+                  <p className='p-4 2xl:p-8 '>{item.desc}</p>
+                  <span className='text-xl font-bold' >${item.price}</span>
+                  <button className='bg-[#04979e] text-white p-2 rounded-md' onClick={() => router.push(`/product/${item._id}`)} >ADD TO CART</button>
+                </div>
+              </div>
+              )
+            }
+          })}
+        </div>
+      </div>
+    )
+  }
+
 }
 
 export default Featured
